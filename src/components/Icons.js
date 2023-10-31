605b815841aef24e7ff413ad1b22a2b64239ba69
ValentinDorigoSAX <ValentinDorigoSax@gmail.com>
2023-10-31 19:34:04 -0300

    3

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,50 +1,53 @@
 const Icons = (icon) => {
-  switch (icon) {
+    let iconPath = 'src/assets/icons/clear-day.svg'; // Valor por defecto
+  
+    switch (icon) {
       case 'Thunderstorm':
-          icon='src/assets/icons/thunderstorms-rain.svg'
-          console.log("TORMENTA")
-          break;    
+        iconPath = 'src/assets/icons/thunderstorms-rain.svg';
+        console.log("TORMENTA");
+        break;
       case 'Drizzle':
-          icon='/src/assets/icons/drizzle.svg'
-          console.log('LLOVIZNA');
-          break;
+        iconPath = 'src/assets/icons/drizzle.svg';
+        console.log('LLOVIZNA');
+        break;
       case 'Rain':
-          icon='/src/assets/icons/rain.svg'
-          console.log('LLUVIA');
-          break;
+        iconPath = 'src/assets/icons/rain.svg';
+        console.log('LLUVIA');
+        break;
       case 'Snow':
-          icon='/src/assets/icons/snowy.svg'
-          console.log('NIEVE');
-          break;                        
+        iconPath = 'src/assets/icons/snowy.svg';
+        console.log('NIEVE');
+        break;
       case 'Clear':
-          icon='/src/assets/icons/clear-day.svg'
-          console.log('LIMPIO');
-          break;
+        iconPath = 'src/assets/icons/clear-day.svg';
+        console.log('LIMPIO');
+        break;
       case 'Atmosphere':
-          icon='/src/assets/icons/weather.svg'
-          console.log('ATMOSFERA');
-          break;  
+        iconPath = 'src/assets/icons/weather.svg';
+        console.log('ATMOSFERA');
+        break;
       case 'Clouds':
-          icon='/src/assets/icons/fog.svg'
-          console.log('NUBES');
-          break;  
+        iconPath = 'src/assets/icons/fog.svg';
+        console.log('NUBES');
+        break;
       case 'Fog':
-          icon='/src/assets/icons/fog.svg'
-          console.log('NUBES');
-          break;    
+        iconPath = 'src/assets/icons/fog.svg';
+        console.log('NUBES');
+        break;
       case 'Haze':
-          icon='/src/assets/icons/haze.svg'
-          console.log('BRUMAS');
-          break;   
+        iconPath = 'src/assets/icons/haze.svg';
+        console.log('BRUMAS');
+        break;
       case 'Smoke':
-          icon='/src/assets/icons/smoke.svg'
-          console.log('HUMO');
-          break;      
+        iconPath = 'src/assets/icons/smoke.svg';
+        console.log('HUMO');
+        break;
       default:
-          icon='/src/assets/icons/clear-day.svg'
-          console.log('LIMPIO');    
-  }
-return icon
-}
-
-export default Icons
+        console.log('Icono no reconocido, usando valor por defecto (LIMPIO)');
+    }
+  
+    return iconPath;
+  };
+  
+  export default Icons;
+  
